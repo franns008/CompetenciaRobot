43b209f81e7a0c0c8a0b57798446d0a235c7a5a4
Damián Bicocchi <pdamianbicocchi@gmail.com>
2025-08-27 18:39:52 -0300

    podemos empezar con la tactica

--- src/Rip912/futbolChampagne.java
+++ src/Rip912/futbolChampagne.java
@@ -1,5 +1,27 @@
 package Rip912;
 
+import robocode.JuniorRobot;
+
 public class futbolChampagne implements Estrategia{
 
+    private JuniorRobot robot;
+
+    public futbolChampagne(SapardoMarcelo robot) {
+        this.robot = robot;
+    }
+
+    @Override
+    public void onScannedRobot() {
+
+    }
+
+    @Override
+    public void onHitByBullet() {
+
+    }
+
+    @Override
+    public void onHitWall() {
+
+    }
 }
